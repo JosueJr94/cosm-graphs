e107f1fca97518fdb1df514053bd4ba7776ababb
shimobayashi <akimasa@shimobayashi.name>
2013-02-02 12:15:22 +0900

    1分毎にグラフを自動更新するようにした

---
+++ useful.js
@@ -0,0 +1,24 @@
+function getImageUrl(klass) {
+    var urls = {
+        'temperature':'https://api.cosm.com/v2/feeds/89487/datastreams/temperature.png?width=1000&height=180&colour=%23f15a24&duration=9hours&title=Temperature&stroke_size=1&show_axis_labels=true&detailed_grid=true&scale=manual&min=15&max=25&timezone=Tokyo',
+        'humidity':'https://api.cosm.com/v2/feeds/89487/datastreams/humidity.png?width=1000&height=180&colour=%23f15a24&duration=9hours&title=Humidity&stroke_size=1&show_axis_labels=true&detailed_grid=true&scale=manual&min=30&max=60&timezone=Tokyo',
+        'pressure':'https://api.cosm.com/v2/feeds/89487/datastreams/pressure.png?width=1000&height=180&colour=%23f15a24&duration=9hours&title=Pressure&stroke_size=1&show_axis_labels=true&detailed_grid=true&scale=manual&min=99000&max=104000&timezone=Tokyo',
+        'tremor':'https://api.cosm.com/v2/feeds/89487/datastreams/bed-tremor.png?width=1000&height=180&colour=%23f15a24&duration=9hours&title=Tremor&stroke_size=1&show_axis_labels=true&detailed_grid=true&scale=manual&min=0&max=0.25&timezone=Tokyo',
+        'sound-level':'https://api.cosm.com/v2/feeds/89487/datastreams/bed-sound-level.png?width=1000&height=180&colour=%23f15a24&duration=9hours&title=Sound%20Level&stroke_size=1&show_axis_labels=true&detailed_grid=true&scale=manual&min=23&max=33&timezone=Tokyo',
+    };
+    var timestamp = new Date().getTime();
+    return urls[klass] + '&dummy=' + timestamp;
+}
+
+function updateGraphs() {
+    $('img').each(function() {
+        var $self = $(this);
+        $self.attr('src', getImageUrl($self.attr('class')));
+    });
+    console.log('updated');
+}
+
+$(function() {
+    updateGraphs();
+    setInterval(updateGraphs, 60 * 1000);
+});
